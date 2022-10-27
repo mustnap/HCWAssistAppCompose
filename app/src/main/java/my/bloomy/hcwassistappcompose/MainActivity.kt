8e6e7ded62c7c0914687efbdd131be72cfc60775
Munsif <mustnap@gmail.com>
2022-10-27 15:27:00 +0800

    managed to add compose

--- app/src/main/java/my/bloomy/hcwassistappcompose/MainActivity.kt
+++ app/src/main/java/my/bloomy/hcwassistappcompose/MainActivity.kt
@@ -10,20 +10,19 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import my.bloomy.hcwassistappcompose.ui.theme.HCWAssistAppComposeTheme
 
 class MainActivity : ComponentActivity() {
+    private lateinit var navController: NavHostController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             HCWAssistAppComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                navController = rememberNavController()
+//                SetupNavGraph(navController = navController)
             }
         }
     }
